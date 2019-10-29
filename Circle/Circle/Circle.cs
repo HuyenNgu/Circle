0bebbf3101a80171a77bb7fbfa2725f5fd6a3e8b
HuyenNgu <yurikawsaoki@gmail.com>
2019-10-29 15:14:49 +0700

    Circle

---
+++ Circle/Circle/Circle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_Circle
+{
+    public class Circle
+    {
+        private double radius;
+        private string _color;
+        
+        public string Color
+        {
+            get
+            {
+                return this._color;
+            }
+            set
+            {
+                this._color = value;
+            }
+        }
+
+        public Circle()
+        {
+            radius = 1.0;
+            Color = "red";
+        }
+
+        public Circle(double r, string c)
+        {
+            radius = 1.0;
+            Color = "red";
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+
+        }
+        public double getArea1()
+        {
+            return 2*radius*Math.PI;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public void SetRadius(double newRadius)
+        {
+            radius = newRadius;
+        }
+
+        //public override string ToString()
+        //{
+        //    return string.Format("the circle has radius of {0} and color of {1}", radius, Color);
+        //}
+
+        public override string ToString()
+        {
+            return string.Format("the circle has radius of {0}", radius);
+        }
+
+        public double Circumference()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Drawing_Circle;
+namespace DrawCircle
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+
+            Console.WriteLine("The circle has radius of {0} and area of {1}", c1.GetRadius(), c1.getArea());
+            
+            Circle c2 = new Circle();
+            Console.WriteLine("The circle has radius of {0} and area of {1}", c2.GetRadius(), c2.getArea());
+            
+            Circle c3 = new Circle(3, "Orange");
+            Console.WriteLine("The circle has radius of {0} and area of {1}", c3.GetRadius(), c3.Color);
+
+            Console.WriteLine(c3.ToString());
+
+        }
+    }
+}
